fbff80bbad6a835f86128bb4051ebe4a91aa9695
Rahaf Tabbaa <rahaf27ta@gmail.com>
2025-01-21 18:07:01 +0000

    Kunde erstellen

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,7 +9,8 @@ export class AuthService {
   
 
   private restaurantApiUrl= '/api/v1/restaurant';
-  private kundeApiUrl = '/api/v1/user';
+  private kundeApiUrl= '/api/v1/kunde';
+  private userApiUrl = '/api/v1/user';
   
   
  // Ändern Sie den Pfad entsprechend Ihrem Backend
@@ -23,12 +24,17 @@ export class AuthService {
     return this.http.post(`${this.restaurantApiUrl}/erstellen`, restaurantData);
   }
 
+  signupKunde(kundeData: any): Observable<any> {
+    
+    return this.http.post(`${this.kundeApiUrl}/erstellen`, kundeData);
+  }
+
   // Für Login (Annahme, dass Login Endpoint existiert oder ähnliche Methode)
   login(credentials: any): Observable<any> {
     
     console.log(' credentials:  ', credentials);
 
-    return this.http.post(`${this.kundeApiUrl}/login`, credentials); // Ändern Sie den Pfad entsprechend Ihrem Backend
+    return this.http.post(`${this.userApiUrl}/login`, credentials); // Ändern Sie den Pfad entsprechend Ihrem Backend
 
     
      // Ändern Sie den Pfad entsprechend Ihrem Backend
--- src/app/singup/singup.component.ts
+++ src/app/singup/singup.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-singup',
-  imports: [RouterLink,RouterOutlet],
   templateUrl: './singup.component.html',
+  standalone:true,
+    imports: [CommonModule, FormsModule, RouterLink],
   styleUrl: './singup.component.css'
 })
 export class SingupComponent {
 
+  kunde: any = { vorname: '', nachname:'', email:'', strasse: '', plz: '', password: '', ort:'' };  // Für die Registrierung
+
+   constructor(private authService: AuthService) {}
+
+  signup(formValue: any) {
+    console.log('formValue: ', formValue);
+    this.authService.signupKunde(formValue).subscribe({
+      next: (response) => console.log('Registrierung erfolgreich', response),
+      error: (error) => console.log('Fehler bei der Registrierung', error)
+    });
+  }
 }
