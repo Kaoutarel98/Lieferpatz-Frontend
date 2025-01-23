393207774b80bc8ef3c3fadd987b8e872f786039
Kaoutarel98 <kaoutarel1998@outlook.de>
2025-01-23 13:36:51 +0100

    refactor: update credentials structure in partner login component

--- src/app/partner-login/partner-login.component.ts
+++ src/app/partner-login/partner-login.component.ts
@@ -13,8 +13,8 @@ import { Router } from '@angular/router';
   styleUrl: './partner-login.component.css'
 })
 export class PartnerLoginComponent {
-  user: any = { name: '',Email:'', strasse: '', plz: '',ort:'',beschreibung: '', bild: , passwort: '' };  // Für die Registrierung
-  credentials = { username: '', loginPassword: '' };
+  user: any = { name: '',Email:'', strasse: '', plz: '',ort:'',beschreibung: '', bild:'' , passwort: '' };  // Für die Registrierung
+  credentials = { email: '', password: '' };
   constructor(private authService: AuthService, private router: Router) {}
  
 
